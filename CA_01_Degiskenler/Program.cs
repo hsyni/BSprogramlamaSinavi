75978e15cd400ad60ac8a0c1d97edc16a0f7cdcc
hsynkzn <h.ozgur.kazan@gmail.com>
2021-06-07 09:53:13 +0300

    Add project files.

---
+++ CA_01_Degiskenler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_01_Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
